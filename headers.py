78e2f10d37cd8d5f21858788fd09deaf9279bee3
Recep <recep.firincioglu@gmail.com>
2021-01-28 20:54:39 +0300

    header class created

---
+++ headers.py
@@ -0,0 +1,11 @@
+class Headers:
+    # define headers
+    rmls = 'SollRückmeldetermin Leitstand'      # dd.mm.YYYY
+    kTermin = 'Konstruktionstermin Soll'        # dd.mm.YYYY
+    kem = 'Dokument'                            # clean WAR,EBS & write HKB, GEN, GEL ; SU_?, CO_?
+    combinedColumns = 'Document Status'         # Combined docStatus & orderPhase
+    docStatus = 'Dokumentstatus'                # 46, 47, 42
+    orderPhase = 'Auftragsphase'                # E, T
+    pivotTableItem1 ='Gecikme Nedeni'
+    pivotTableItem2 ='Alınacak Aksiyon'
+    pivotTableItem3 ='Çalışma Tipi'
