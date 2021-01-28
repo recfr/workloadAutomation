104eea063f202fadab96d25c397e818e24e67627
Recep <recep.firincioglu@gmail.com>
2021-01-28 20:56:38 +0300

    Status/Phase columns are combined & cleaned

--- workloadAutomation.py
+++ workloadAutomation.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import headers
 import datetime
 import openpyxl
+
+# define headers
+myHeader = headers.Headers()
 
 # def today():
 #     date = datetime.date.today()
@@ -8,50 +12,43 @@ import openpyxl
 #     return formatedDate
 #     # after3days = formatedDate[:2]
 #     # # print(int(after3days)+3)
-#
+
+#clean KEM numbers
 def rowCleaner_KEM(columnName):
     for row in df[columnName]:
-        if row[:3] == "WAR" :
+        if row[:3] == "WAR" or row[:3] == "EBS":
             rowIndex = next(iter(df[df[columnName] == row].index), 'no match')
             df.drop(rowIndex, inplace=True)
 
+#Combine documentStatus and orderPhase
+def combineColumns(sheetName, firstColumn, secondColumn, position):
+    combinedColumn = sheetName[firstColumn] + "/" + sheetName [secondColumn]
+    sheetName.insert(position, headers.Headers.combinedColumns, combinedColumn)
+    del sheetName[firstColumn]
+    del sheetName[secondColumn]
 
-# def rowCleaner_Status(columnNameStatus, columnNamePhase):
-#     for row in df[columnNameStatus]:
-#         if row == 46:
-#             print('kirk alti')
-#             # print(next(iter(df[df[columnNamePhase] == row].index), 'no match'))
-#             for row in df[columnNamePhase]:
-#                 if row[:1] == 'T':
-#                     rowIndex = next(iter(df[df[columnNamePhase] == row].index), 'no match')
-#                     df.drop(rowIndex, inplace=True)
-#                 else:
-#                     continue
-#         else:
-#             continue
-
+def rowCleaner_docStatus(columnNameStatus):
+    for row in df[columnNameStatus]:
+        if str(row) == "46/T":
+            rowIndex = next(iter(df[df[columnNameStatus] == row].index), 'no match')
+            df.drop(rowIndex, inplace=True)
 
 # read excel file
 dailyWorkload = 'dailyworkload.xlsx'
 df = pd.read_excel(dailyWorkload, sheet_name='Sheet1')
 
 # adding pivot_table arguments
-df['Gecikme Nedeni'] = 0
-df['Alınacak Aksiyon'] = 0
-df['Çalışma Tipi'] = 0
-
-# define headers
-rmls = 'SollRückmeldetermin Leitstand'      # dd.mm.YYYY
-kTermin = 'Konstruktionstermin Soll'        # dd.mm.YYYY
-kem = 'Dokument'                            # clean WAR, write HKB, GEN, GEL
-# docStatus = 'Dokumentstatus'                # 46, 47, 42
-# orderPhase = 'Auftragsphase'                # E, T
+df[headers.Headers().pivotTableItem1] = 0
+df[headers.Headers().pivotTableItem2] = 0
+df[headers.Headers().pivotTableItem3] = 0
 
 
-rowCleaner_KEM(kem)
-# rowCleaner_Status(docStatus, orderPhase)
+combineColumns(df, headers.Headers.docStatus, headers.Headers.orderPhase, 5)
+rowCleaner_KEM(headers.Headers.kem)
+rowCleaner_docStatus(headers.Headers.combinedColumns) # cleaning test docStatus correct
 
+# print(df[headers.Headers.rmls][40:55])
 
-# writer = pd.ExcelWriter('edited_Workload.xlsx')
-# df.to_excel(writer, 'Sheet1')
-# writer.save()
+writer = pd.ExcelWriter('edited_Workload.xlsx')
+df.to_excel(writer, 'Sheet1')
+writer.save()
