6b3c6943f4b913ac4f663d90caa5c5ad1a717272
Recep <recep.firincioglu@gmail.com>
2021-08-05 20:51:01 +0300

    Refactor

--- headers.py
+++ headers.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 import datetime
+import pandas
+
 
 class Headers:
 
@@ -8,114 +10,115 @@ class Headers:
         self.tempRMLS = 'daysDiff_RMLS'
         self.tempTermin = 'daysDiff_Termin'
         self.combinedColumns = 'Document Status'  # Combined docStatus & orderPhase
+        self.combinedDates = 'combi-date'
 
+        # define headers
         self.rmls = 'SollRückmeldetermin Leitstand'  # dd.mm.YYYY
         self.kTermin = 'Konstruktionstermin Soll'  # dd.mm.YYYY
         self.docType = 'Dokument'  # clean WAR,EBS & write HKB, GEN, GEL ; SU_?, CO_?
         self.bbNummer = 'BB-Nummer'
         self.docStatus = 'Dokumentstatus'  # 46, 47, 42, FG
         self.orderPhase = 'Auftragsphase'  # E, T
+        self.sachbearbeiter = 'Sachbearbeiter'
         self.pivotTableItem1 = 'Gecikme Nedeni'
         self.pivotTableItem2 = 'Alınacak Aksiyon'
         self.pivotTableItem3 = 'Çalışma Tipi'
 
-    def addPivotTableHeaders(self, sheetName):
-        sheetName[self.pivotTableItem1] = " "
-        sheetName[self.pivotTableItem2] = " "
-        sheetName[self.pivotTableItem3] = " "
+    def addPivotTableHeaders(self, sheetname):
+        sheetname[self.pivotTableItem1] = " "
+        sheetname[self.pivotTableItem2] = " "
+        sheetname[self.pivotTableItem3] = " "
 
-    def timeDiff(self, sheetName):
-        daysDiff_Termin = sheetName[self.kTermin] - datetime.datetime.now()
-        sheetName.insert(0, self.tempTermin, daysDiff_Termin.dt.days)
-        daysDiff_RMLS = sheetName[self.rmls] - datetime.datetime.now()
-        sheetName.insert(0, self.tempRMLS, daysDiff_RMLS.dt.days)
+    def timeDiff(self, sheetname):
+        # demoDate = datetime.datetime(2021, 04, 15).strftime('%d-%m-%Y')
+        currentDate = datetime.datetime.now()
+        daysDiff_Termin = sheetname[self.kTermin] - currentDate
+        sheetname.insert(0, self.tempTermin, daysDiff_Termin.dt.days)
+        daysDiff_RMLS = sheetname[self.rmls] - currentDate
+        sheetname.insert(0, self.tempRMLS, daysDiff_RMLS.dt.days)
 
-    def cleanByDate(self, sheetName):
+    def cleanByDate(self, sheetname):
         self.dayName = datetime.date.today().strftime('%A')
 
         if self.dayName == 'Monday' or self.dayName == 'Tuesday':
-            for row in sheetName[self.tempRMLS]:
+            for row in sheetname[self.tempRMLS]:
                 if row != None and row >= 3:
-                    rowIndex = next(iter(sheetName[sheetName[self.tempRMLS] == row].index), 'no match')
-                    sheetName.drop(rowIndex, inplace=True)
-            for row in sheetName[self.tempTermin]:
+                    rowIndex = next(iter(sheetname[sheetname[self.tempRMLS] == row].index), 'no match')
+                    sheetname.drop(rowIndex, inplace=True)
+            for row in sheetname[self.tempTermin]:
                 if row != None and row >= 3:
-                    rowIndex = next(iter(sheetName[sheetName[self.tempTermin] == row].index), 'no match')
-                    sheetName.drop(rowIndex, inplace=True)
+                    rowIndex = next(iter(sheetname[sheetname[self.tempTermin] == row].index), 'no match')
+                    sheetname.drop(rowIndex, inplace=True)
 
         elif self.dayName == 'Wednesday' or self.dayName == 'Thursday' or self.dayName == 'Friday':
-            for row in sheetName[self.tempRMLS]:
+            for row in sheetname[self.tempRMLS]:
                 if row != None and row > 4:
-                    rowIndex = next(iter(sheetName[sheetName[self.tempRMLS] == row].index), 'no match')
-                    sheetName.drop(rowIndex, inplace=True)
-            for row in sheetName[self.tempTermin]:
+                    rowIndex = next(iter(sheetname[sheetname[self.tempRMLS] == row].index), 'no match')
+                    sheetname.drop(rowIndex, inplace=True)
+            for row in sheetname[self.tempTermin]:
                 if row != None and row > 4:
-                    rowIndex = next(iter(sheetName[sheetName[self.tempTermin] == row].index), 'no match')
-                    sheetName.drop(rowIndex, inplace=True)
-        del sheetName[self.tempRMLS]
-        del sheetName[self.tempTermin]
+                    rowIndex = next(iter(sheetname[sheetname[self.tempTermin] == row].index), 'no match')
+                    sheetname.drop(rowIndex, inplace=True)
 
-
-
-    def rowCleaner_KEM(self, sheetName):
-        for row in sheetName[self.docType]:
+    def rowCleaner_KEM(self, sheetname):
+        for row in sheetname[self.docType]:
             if row[:3] == "WAR" or row[:3] == "EBS":
-                rowIndex = next(iter(sheetName[sheetName[self.docType] == row].index), 'no match')
-                sheetName.drop(rowIndex, inplace=True)
+                rowIndex = next(iter(sheetname[sheetname[self.docType] == row].index), 'no match')
+                sheetname.drop(rowIndex, inplace=True)
 
-    def combineColumns(self, sheetName):
+    def combineColumns(self, sheetname):
         # Combine documentStatus and orderPhase
-        combinedColumn = sheetName[self.docStatus].apply(str) + "/" + sheetName[self.orderPhase]
-        sheetName.insert(5, self.combinedColumns, combinedColumn)
-        del sheetName[self.docStatus]
-        del sheetName[self.orderPhase]
+        combinedColumn = sheetname[self.docStatus].apply(str) + "/" + sheetname[self.orderPhase]
+        sheetname.insert(5, self.combinedColumns, combinedColumn)
+        del sheetname[self.docStatus]
+        del sheetname[self.orderPhase]
 
-    def rowCleaner_docStatus(self, sheetName):
-        for row in sheetName[self.combinedColumns]:
+    def rowCleaner_docStatus(self, sheetname):
+        for row in sheetname[self.combinedColumns]:
             if str(row) == "46/T":
-                rowIndex = next(iter(sheetName[sheetName[self.combinedColumns] == row].index), 'no match')
-                sheetName.drop(rowIndex, inplace=True)
+                rowIndex = next(iter(sheetname[sheetname[self.combinedColumns] == row].index), 'no match')
+                sheetname.drop(rowIndex, inplace=True)
 
-    def cleanBy_BBnummer(self, sheetName):
-        sheetName[self.bbNummer] = sheetName[self.bbNummer].fillna('-')
-        for row in sheetName[self.bbNummer]:
+    def cleanBy_BBnummer(self, sheetname):
+        sheetname[self.bbNummer] = sheetname[self.bbNummer].fillna('-')
+        for row in sheetname[self.bbNummer]:
             if row == '-':
-                rowIndex = next(iter(sheetName[sheetName[self.bbNummer] == row].index), 'no match')
-                sheetName.drop(rowIndex, inplace=True)
+                rowIndex = next(iter(sheetname[sheetname[self.bbNummer] == row].index), 'no match')
+                sheetname.drop(rowIndex, inplace=True)
 
-    def rowMark_GEL(self, sheetName):
-        for row in sheetName[self.docType]:
+    def rowMark_GEL(self, sheetname):
+        for row in sheetname[self.docType]:
             if row[:3] == "GEL":
-                rowIndex = next(iter(sheetName[sheetName[self.docType] == row].index), 'no match')
-                sheetName.loc[rowIndex, self.pivotTableItem3] = 'AKT'
+                rowIndex = next(iter(sheetname[sheetname[self.docType] == row].index), 'no match')
+                sheetname.loc[rowIndex, self.pivotTableItem3] = 'AKT'
 
-    def rowMark_HKB(self, sheetName):
-        for row in sheetName[self.docType]:
+    def rowMark_HKB(self, sheetname):
+        for row in sheetname[self.docType]:
             if row[:3] == "HKB" or row[:3] == "GEN" or row[:3] == "KAT" or row[:3] == 'CO_' or row[:3] == 'SU_':
-                rowIndex = next(iter(sheetName[sheetName[self.docType] == row].index), 'no match')
-                sheetName.loc[rowIndex, self.pivotTableItem3] = 'RMLS'
+                rowIndex = next(iter(sheetname[sheetname[self.docType] == row].index), 'no match')
+                sheetname.loc[rowIndex, self.pivotTableItem3] = 'RMLS'
 
-    def rowMark_Mitteilung(self, sheetName):
-        for index, value in sheetName[self.docType].items():
+    def rowMark_Mitteilung(self, sheetname):
+        for index, value in sheetname[self.docType].items():
             if value[:2] == "ME" and len(value) == 12:
-                sheetName.loc[index, self.pivotTableItem3] = 'Bildiri'
+                sheetname.loc[index, self.pivotTableItem3] = 'Bildiri'
 
-    def workingDays(self, sheetName):
-        weekdays_rmls_list = sheetName[self.rmls].dt.day_name()
-        weekdays_termin_list = sheetName[self.kTermin].dt.day_name()
-        sheetName['combinedDays'] = weekdays_termin_list.fillna('') + weekdays_rmls_list.fillna('')
+    def workingDays(self, sheetname):
+        weekdays_rmls_list = sheetname[self.rmls].dt.day_name()
+        weekdays_termin_list = sheetname[self.kTermin].dt.day_name()
+        sheetname['combinedDays'] = weekdays_termin_list.fillna('') + weekdays_rmls_list.fillna('')
 
-        sheetName['combinedDays'] = sheetName['combinedDays'].replace('Monday', 'Pazartesi Çalışılacak')
-        sheetName['combinedDays'] = sheetName['combinedDays'].replace('Tuesday', 'Salı Çalışılacak')
-        sheetName['combinedDays'] = sheetName['combinedDays'].replace('Wednesday', 'Çarşamba Çalışılacak')
-        sheetName['combinedDays'] = sheetName['combinedDays'].replace('Thursday', 'Perşembe Çalışılacak')
-        sheetName['combinedDays'] = sheetName['combinedDays'].replace('Friday', 'Cuma Çalışılacak')
-        sheetName['combinedDays'] = sheetName['combinedDays'].replace('Saturday', 'Cuma Çalışılacak')
-        sheetName['combinedDays'] = sheetName['combinedDays'].replace('Sunday', 'Cuma Çalışılacak')
+        sheetname['combinedDays'] = sheetname['combinedDays'].replace('Monday', 'Pazartesi Çalışılacak')
+        sheetname['combinedDays'] = sheetname['combinedDays'].replace('Tuesday', 'Salı Çalışılacak')
+        sheetname['combinedDays'] = sheetname['combinedDays'].replace('Wednesday', 'Çarşamba Çalışılacak')
+        sheetname['combinedDays'] = sheetname['combinedDays'].replace('Thursday', 'Perşembe Çalışılacak')
+        sheetname['combinedDays'] = sheetname['combinedDays'].replace('Friday', 'Cuma Çalışılacak')
+        sheetname['combinedDays'] = sheetname['combinedDays'].replace('Saturday', 'Cuma Çalışılacak')
+        sheetname['combinedDays'] = sheetname['combinedDays'].replace('Sunday', 'Cuma Çalışılacak')
 
-        sheetName[self.pivotTableItem1] = sheetName['combinedDays']
-        sheetName[self.pivotTableItem2] = sheetName['combinedDays']
-        del sheetName['combinedDays']
+        sheetname[self.pivotTableItem1] = sheetname['combinedDays']
+        sheetname[self.pivotTableItem2] = sheetname['combinedDays']
+        del sheetname['combinedDays']
 
     def daySwitch(self, argument):
         self.switcher = {
@@ -129,32 +132,51 @@ class Headers:
         }
         return self.switcher.get(argument, "Invalid day")
 
-    def rowMark_Fehler(self, sheetName):
-        for row in sheetName[self.docType]:
+    def rowMark_Fehler(self, sheetname):
+        for row in sheetname[self.docType]:
             if len(row) > 12 and row[:2] == 'ME':
-                rowIndex = next(iter(sheetName[sheetName[self.docType] == row].index), 'no match')
-                sheetName.loc[rowIndex, self.pivotTableItem1] = f'{self.daySwitch(self.dayName)} Çalışılacak'
-                sheetName.loc[rowIndex, self.pivotTableItem2] = f'{self.daySwitch(self.dayName)} Çalışılacak'
-                sheetName.loc[rowIndex, self.pivotTableItem3] = 'Fehler'
+                rowIndex = next(iter(sheetname[sheetname[self.docType] == row].index), 'no match')
+                sheetname.loc[rowIndex, self.pivotTableItem1] = f'{self.daySwitch(self.dayName)} Çalışılacak'
+                sheetname.loc[rowIndex, self.pivotTableItem2] = f'{self.daySwitch(self.dayName)} Çalışılacak'
+                sheetname.loc[rowIndex, self.pivotTableItem3] = 'Fehler'
 
-    def splitStatus(self, sheetName):
+    def splitStatus(self, sheetname):
         status_list = []
         phases_list = []
-        excel_status = sheetName[self.combinedColumns]
+        excel_status = sheetname[self.combinedColumns]
 
         for status in excel_status:
             status, phases = status.split('/', 1)
             status_list.append(status)
             phases_list.append(phases)
 
-        sheetName.insert(5, self.docStatus, status_list)
-        sheetName.insert(6, self.orderPhase, phases_list)
+        sheetname.insert(5, self.docStatus, status_list)
+        sheetname.insert(6, self.orderPhase, phases_list)
 
-    def rowMark_Status47(self, sheetName):
-        for index, value in sheetName[self.docStatus].items():
-            # print(f"Index : {index}, Value : {value}")
+    def rowMark_Status47(self, sheetname):
+        for index, value in sheetname[self.docStatus].items():
             if value == "47":
-                sheetName.loc[index, self.pivotTableItem1] = f"{value}'de"
-                sheetName.loc[index, self.pivotTableItem2] = f"{value}'de"
-                sheetName.loc[index, self.pivotTableItem3] = f"{value}'de"
-        del sheetName[self.combinedColumns]
+                sheetname.loc[index, self.pivotTableItem1] = f"{value}'de"
+                sheetname.loc[index, self.pivotTableItem2] = f"{value}'de"
+                sheetname.loc[index, self.pivotTableItem3] = f"{value}'de"
+        del sheetname[self.combinedColumns]
+
+    def isOpenScope(self, sheetname):
+        for index, value in sheetname[self.tempRMLS].items():
+            if value != None and value < -1:
+                sheetname.loc[index, self.pivotTableItem1] = 'Açık Kapsam'
+                sheetname.loc[index, self.pivotTableItem2] = 'Açık Kapsam'
+                sheetname.loc[index, self.pivotTableItem3] = 'Kontrol Edilecek'
+
+        del sheetname[self.tempRMLS]
+        del sheetname[self.tempTermin]
+
+    def create_PivotTable(self, sheetname):
+        pivot_table = pandas.pivot_table(sheetname,
+                                          index=self.sachbearbeiter,
+                                          columns=[self.pivotTableItem2, self.pivotTableItem3],
+                                          values=self.orderPhase,
+                                          aggfunc='count',
+                                          margins=True,
+                                          margins_name='Toplam')
+        return pivot_table
--- outlook.py
+++ outlook.py
@@ -15,32 +15,6 @@ class EmailSender:
         calendar_week = datetime.date.today().isocalendar()[1]
         outlook = client.Dispatch('outlook.application')
 
-        html_body = "<html>" \
-               "<head></head>" \
-               "<body>" \
-               "<p>Merhaba Arkadaşlar,<br></p>" \
-               "<p>RMLS tarihi gelen iş yüklerini bugün tamamlamanız ricasıyla, iyi çalışmalar dilerim.</p>" \
-               "<div>image_source(graphic)" \
-               "<img src{}></img>" \
-               "</div>" \
-               "<p><u><b>ANALİZ:</b></u></p>" \
-               "<p><ol></p>" \
-               "<li> KW%(cw_early)i ve KW%(cw_today)i genel olarak çalışma kapasitemize uygun çalışılmıştır.</li>" \
-               "<li> Şu an iş yükünde “ ” Fehler, “ “ Açık Kapsam,  “ “ RMLS,  “ “ AKT ve  “ “ Bildiri bulunmaktadır.</li>" \
-               "</ol>" \
-               "<div>image_source(pivot_table)" \
-               "<img src{}></img>" \
-               "</div>" \
-               "<b><p>ÖNEMLİ : </p></b> " \
-               "<b>“Açık Kapsam: Kontrol Edilecek”</b> sütununda 47’den dönen, geçmiş tarihli, satış nachtragı ya da başka herhangi" \
-               "sebepten açılan kapsamlar olabilir. Ekteki excel dokümanında “Sheet1” isimli sayfada, “Çalışma Tipi” sütununda " \
-               "“Kontrol Edilecek” olarak belirtilen çalışmaları kontrol etmenizi rica ederim. " \
-               "<b><p>NOT: Lütfen yetişemeyecek kapsamlar hakkında koordinatörümüzü bilgilendirin. </p></b>" \
-               "</body>" \
-               "</html> " % {"cw_early": int(calendar_week) - 1,
-                             "cw_today": int(calendar_week)
-                             }
-
         # excel = client.Dispatch('Excel.application')
         # wb = excel.Workbooks.Open(workBook_path)
         # sheet = wb.Sheets['Pivot Table']
@@ -48,6 +22,35 @@ class EmailSender:
         # copyrange = sheet.Range('A1:J13')
         # copyrange.CopyPicture(Appearance=1, Format=2)
         # ImageGrab.grabclipboard().save('paste.png')
+
+        html_body = """
+                <html>
+                  <head></head>
+                  <body>
+                  	<font color="Black" face="Calibri">
+                    <p>Merhaba arkadaşlar,<br>
+                       RMLS tarihi gelen iş yüklerini bugün tamamlamanız ricasıyla, iyi çalışmalar dilerim.<br>
+                       
+                        <div>image_source(graphic) <img src{}></img> </div>
+                        <p><u><b>ANALİZ:</b></u></p>
+                        <p><ol></p>
+                        <li> KW%(cw_early)i ve KW%(cw_today)i genel olarak çalışma kapasitemize uygun çalışılmıştır.</li>
+                        <li> Şu an iş yükünde “ ” Fehler, “ “ Açık Kapsam,  “ “ RMLS,  “ “ AKT ve  “ “ Bildiri bulunmaktadır.</li></ol>
+                        
+                        <div>image_source(pivot_table)" <img src{}></img>" </div>
+                        <b><p>ÖNEMLİ : </p></b>
+                        
+                        <b>“Açık Kapsam: Kontrol Edilecek”</b> sütununda <b>47’den dönen, geçmiş tarihli, satış nachtragı</b> ya da başka herhangi 
+                        sebepten açılan kapsamlar olabilir. Ekteki excel dokümanında “Sheet1” isimli sayfada, 
+                        “<b>Çalışma Tipi</b>” sütununda “<b>Kontrol Edilecek</b>” olarak belirtilen çalışmaları kontrol etmenizi rica ederim.
+                        
+                        <b><p>NOT: Lütfen yetişemeyecek kapsamlar hakkında koordinatörümüzü bilgilendirin. </p></b>
+                    </p>
+                    
+                    </font>
+                  </body>
+                </html>
+                """ % {"cw_early": int(calendar_week) - 1, "cw_today": int(calendar_week)}
 
         mail = outlook.CreateItem(0)
         mail.To = self.employeeList
@@ -59,5 +62,7 @@ class EmailSender:
         # attachment = self.desktop + "\\" + workBook_path
         # mail.Attachments.Add(attachment)
 
+
         # mail.Send()
         mail.Display()
+
